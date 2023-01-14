ffaf874e14287db723fea200e5387e7b0880d3cb
Ruban-vela <76084276+Ruban-vela@users.noreply.github.com>
2023-01-14 19:48:55 +0530

    Add files via upload
    newer version of Code

--- folder_perm.ps1
+++ folder_perm.ps1
@@ -1,14 +1,18 @@
 ﻿$primaryuser = (Get-ItemProperty -path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\DataStore\S-1-5-21-1725562737-1827328992-814387261-1001\0").'szName'
 $apppath="C:\Program Files\Google\Chrome\Application\chrome.exe"
 $currentuser= whoami
-$foldepath="C:\WhatsApp Documents"
+$folderpath="C:\WhatsApp Documents"
 
-function setpermission ($folder,$groupname,$permissionlevel)
+function set-permission 
 {
-$acl = get-acl $folder
-$accessrule= New-Object System.Security.AccessControl.FileSystemAccessRule($groupname,$permissionlevel,"Allow")
-$acl.SetAccessRule($accessrule)
-set-acl -path $folder -AclObject $acl
+param($folder,$groupname,$permissionlevel)
+
+$setp= "$Groupname"+':' +"$permissionlevel"
+Write-host $setp
+icacls "$folder" /remove:g $Groupname /t /c
+icacls "$folder"
+icacls.exe "$folder"  /grant "$setp"
+icacls "$folder"
 }
 
 IF (test-path $apppath)
@@ -17,8 +21,8 @@ Write-host " $apppath is accessible" -ForegroundColor Green
 if ($currentuser -eq $primaryuser)
 {
 Write-Host "Current user $currentuser is  Primary user ($primaryuser)" -ForegroundColor Green 
-setpermission($foldepath,"Users","FullControl")
-$acl=get-acl $foldepath
+set-permission $folderpath "Users" "(OI)(CI)(RX)"
+$acl=get-acl $folderpath
 $folderowner=$acl.Owner
 if($folderowner -eq $primaryuser)
 {
@@ -40,13 +44,11 @@ else
 write-host "$apppath is not found or not accessible"
 }
 
-
-
-icacls.exe "C:\WhatsApp Documents"  /grant "Users:(OI)(CI)(RX)"
+<#icacls.exe "C:\WhatsApp Documents"  /grant "Users:(OI)(CI)(RX)"
 icacls "C:\WhatsApp Documents"
 
 
 
 icacls "C:\WhatsApp Documents" /inheritance:d /t /c
 icacls "C:\WhatsApp Documents" /remove:g Users /t /c
-icacls "C:\WhatsApp Documents"
+icacls "C:\WhatsApp Documents"#>
