c014dd76413d475653496f687ab26249f3ee7190
Ruban-vela <76084276+Ruban-vela@users.noreply.github.com>
2024-02-16 00:08:28 +0530

    Add files via upload
    New global search script

---
+++ global_search.ps1
@@ -0,0 +1,9 @@
+﻿$searchdirectories=(get-psdrive | where-object { $_.Root -match ":" } ).Root
+
+$searchpattern = "Ruban"
+
+foreach ( $directory in $searchdirectories ){
+
+$files= Get-ChildItem -Path $directory -Recurse | Where-Object { $_.name -match $searchpattern} 
+$files | Select-Object -Property Name,directory 
+}
